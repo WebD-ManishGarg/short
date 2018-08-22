1e0ea0cfc78e9c56daef5194f26e6a10ed7c593a
Manish <gargmanish097@gmail.com>
2018-08-22 22:06:01 +0530

    g

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express=require("express");
 var app=express();
+const port=process.env.PORT || 3000;
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
 app.get("/",function(req,res){
   res.render("index");
 });
-app.listen(3000,function(err,rak){
+app.listen(port,function(err,rak){
   console.log("Server is started");
 });
